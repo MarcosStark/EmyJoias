dec0fc281e5591ee51d27464ac8bd156c975596b
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-04-28 23:11:57 -0300

    projeto-emyjoias

---
+++ projeto-emyjoias/emyjs.js
@@ -0,0 +1,11 @@
+function start(){
+
+    document.getElementById("email").onclick=function(){
+        alert("example@gmail.com");
+    };
+   
+    document.getElementById("telefone").onclick=function(){
+        alert("00 00000-0000");
+    };
+   
+}
