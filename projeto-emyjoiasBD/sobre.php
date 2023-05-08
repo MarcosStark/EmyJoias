24e8d1ff7286200c4ba586ecabe5c5ba2f36b9bc
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-05-07 22:23:21 -0300

    Update sobre.php

--- projeto-emyjoiasBD/sobre.php
+++ projeto-emyjoiasBD/sobre.php
@@ -60,70 +60,70 @@
     <section id="apresentacao">
       <div class="container">
         <div class="row">
-          <div class="row sobre">
+          <div class="col-md-4">
+            <img src="imagens/img1.jpeg" class="img-responsive img">
+          </div>
+
+          <div class="col-md-8">
+              <p id="p-sobre1" class="p-sobre">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id tempus diam. Aliquam volutpat mollis quam et convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ullamcorper nulla a metus commodo, vitae vestibulum leo gravida. Phasellus ultricies ante augue, ut commodo lacus tempus eu. Fusce nec lacus in justo vulputate dapibus. Vivamus molestie mi ac aliquet facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam ac libero ac odio dignissim efficitur. Sed quis aliquet magna, vitae malesuada urna. Ut consectetur maximus nisl.</p>
+          </div>
+         
+          <div class="col-md-8">
+              <p id="p-sobre2" class="p-sobre">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id tempus diam. Aliquam volutpat mollis quam et convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ullamcorper nulla a metus commodo, vitae vestibulum leo gravida. Phasellus ultricies ante augue, ut commodo lacus tempus eu. Fusce nec lacus in justo vulputate dapibus. Vivamus molestie mi ac aliquet facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam ac libero ac odio dignissim efficitur. Sed quis aliquet magna, vitae malesuada urna. Ut consectetur maximus nisl.</p>
+          </div>
+
+          <div class="col-md-4" id="format-img2">
+            <img src="imagens/img2.jpeg" class="img-responsive img2">
+          </div>
+          
+        </div>
+      </div>
+    </section>
+  
+       <!-- Rodape -->
+    <footer>
+      <div class="container">
+        <div class="row">    
+            <div class="col-md-2">
+                <span class="img-logo">Emy Joias</span>
+            </div>
+
+            <div class="col-md-2">
+            </div>
+
+            <div class="col-md-2">
+              <h4>Fundadora</h4>
+              <ul class="nav">
+                <li><a href="sobre.php">Sobre Emy</a></li>                
+              </ul>
+            </div>
+
+            <div class="col-md-2">
+              <h4>contate-nos</h4>
+              <ul class="nav">
+                <li id="email"><a href="" onclick="start()">Email</a></li>
+                <li id="telefone"><a href="" onclick="start()">Telefone</a></li>
+              </ul>
+            </div>
+
+            <div class="col-md-2">
+            </div>
 
             <div class="col-md-4">
-              <img src="imagens/img1.jpeg" class="img-responsive img">
+              <ul class="nav">
+                <h4>Acompanhe-nos no instagram</h4>
+                <li class="item-rede-social"><a href="https://www.instagram.com/_emyjoias/?hl=bg"><img src="imagens/instagram.png"></a></li>
+              </ul>
             </div>
+        </div><!--/row -->
+      </div><!--/container -->
+    </footer>
 
-            <div class="col-md-8 texto-capa">
-                <p id="p-sobre1" class="p-sobre">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id tempus diam. Aliquam volutpat mollis quam et convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ullamcorper nulla a metus commodo, vitae vestibulum leo gravida. Phasellus ultricies ante augue, ut commodo lacus tempus eu. Fusce nec lacus in justo vulputate dapibus. Vivamus molestie mi ac aliquet facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam ac libero ac odio dignissim efficitur. Sed quis aliquet magna, vitae malesuada urna. Ut consectetur maximus nisl.</p>
-            </div>
-
-          </div><!-- /row -->
-
-            <div class="row sobre">
-              
-             <div class="col-md-8 texto-capa">
-                 <p id="p-sobre2" class="p-sobre">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id tempus diam. Aliquam volutpat mollis quam et convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ullamcorper nulla a metus commodo, vitae vestibulum leo gravida. Phasellus ultricies ante augue, ut commodo lacus tempus eu. Fusce nec lacus in justo vulputate dapibus. Vivamus molestie mi ac aliquet facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam ac libero ac odio dignissim efficitur. Sed quis aliquet magna, vitae malesuada urna. Ut consectetur maximus nisl.</p>
-              </div>
-
-              <div class="col-md-4" id="format-img2">
-                <img src="imagens/img2.jpeg" class="img-responsive img2">
-              </div>
-
-            </div><!-- /row -->
-
-          </div>
-        </section>
-  
-      <!-- Rodape -->
-      <footer id="rodape">
-        <div class="container">
-            <div class="row">
-            
-                <div class="col-md-4">
-                    <span class="img-logo">Emy Joias</span>
-                </div>
-                
-                <div class="col-md-2">
-                    <h4>Fundadora</h4>
-                    <ul class="nav">
-                    <li><a href="sobre.html">Sobre Emy</a></li>                
-                    </ul>
-                </div>
-
-                <div class="col-md-2">
-                    <h4>contate-nos</h4>
-                    <ul class="nav">
-                    <li id="email"><a href="" onclick="start()">Email</a></li>
-                    <li id="telefone"><a href="" onclick="start()">Telefone</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4">
-                    <ul class="nav">
-                    <h4>Acompanhe-nos no instagram</h4>
-                    <li class="item-rede-social"><a href="https://www.instagram.com/_emyjoias/?hl=bg"><img src="imagens/instagram.png"></a></li>
-                    </ul>
-                </div>
-
-            </div><!-- /row -->
-        </div><!-- /container -->
-        </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
     <script src="bootstrap/js/bootstrap.min.js"></script>
+
+    <script language="javascript" src="emyjs.js"></script>
   </body>
 </html>
