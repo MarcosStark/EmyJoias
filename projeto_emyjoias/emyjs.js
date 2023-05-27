62a9de05b885d995ad9fccbad05084f2db75d9ee
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-05-26 21:22:58 -0300

    footer update

--- projeto_emyjoias/emyjs.js
+++ projeto_emyjoias/emyjs.js
@@ -1,11 +1,14 @@
 function start(){
 
-    document.getElementById("email").onclick=function(){
+    document.getElementById("email").onclick = function(){
         alert("example@gmail.com");
-    };
-   
-    document.getElementById("telefone").onclick=function(){
-        alert("00 00000-0000");
-    };
-   
-}
+        }
+        
+        document.getElementById("telefone").onclick = function(){
+            alert("00 00000-0000");
+        };
+       
+}  
+
+
+
